655b38785f4e5edbe2078ded5764a443a8834172
Abhinav Kasam <abhinavkl1412.gmail.com>
2024-10-13 17:57:11 +0530

    Added Blob Storage Service

--- CloudFileManager/Program.cs
+++ CloudFileManager/Program.cs
@@ -1,7 +1,11 @@
+using CloudFileManager.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddSingleton<BlobStorageService>();
 
 var app = builder.Build();
 
---
+++ CloudFileManager/Services/BlobStorageService.cs
@@ -0,0 +1,13 @@
+﻿using Azure.Storage.Blobs;
+
+namespace CloudFileManager.Services
+{
+    public class BlobStorageService
+    {
+        private readonly BlobServiceClient blobServiceClient;
+        public BlobStorageService(IConfiguration configuration) 
+        {
+            blobServiceClient = new BlobServiceClient(configuration.GetConnectionString("AzureStorage"));
+        }
+    }
+}
